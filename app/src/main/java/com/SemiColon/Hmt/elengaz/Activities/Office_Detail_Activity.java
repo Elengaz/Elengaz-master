0d96a04ea3a5daddffe2c5aecb8060bb2fd4b748
Emadmagdy151995Fci <emadmagdi.151995@gmai.com>
2018-03-07 20:26:23 +0200

    initial comment

---
+++ app/src/main/java/com/SemiColon/Hmt/elengaz/Activities/Office_Detail_Activity.java
@@ -0,0 +1,15 @@
+package com.SemiColon.Hmt.elengaz.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.SemiColon.Hmt.elengaz.R;
+
+public class Office_Detail_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_office_detail);
+    }
+}
